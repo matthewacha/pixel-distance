846009803dc758bff109ebc6c304e9e436e94a1b
Matthew Wacha <matthewacha@gmail.com>
2022-01-18 11:29:54 +0300

    add entry file

---
+++ index.ts
@@ -0,0 +1,16 @@
+import main from './main';
+
+let inputString: string = '';
+let inputLines: string[] = [];
+
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin; 
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main(inputLines);
+});
